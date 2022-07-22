133cf089da58c253428b6199323907309997e8bc
Darlley <systembrtt@gmail.com>
2022-07-22 09:48:47 -0400

    component

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,8 +2,6 @@
 
 @section('content')
     <div class="flex justify-center items-center h-screen bg-gradient-to-tr from-primary-500 to-secundary-500">
-        <div class="w-max rounded-full px-4 py-2 bg-opacity-80 backdrop-blur-sm shadow-xl animate-pulse bg-white">
-            <img src="{{ asset('img/logo-adote-um-dev_h-doc.svg') }}" alt="" class="h-16">
-        </div>
+        <x-logo />
     </div>
 @endsection
