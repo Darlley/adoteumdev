21c60a9309ff3b1c03bea53278979d4bfa6370a3
Darlley <systembrtt@gmail.com>
2022-08-12 09:08:51 -0400

    :sparkles: feat: tela de splash screen completa com animação"

--- resources/views/livewire/components/splash-screen.blade.php
+++ resources/views/livewire/components/splash-screen.blade.php
@@ -1,7 +1,16 @@
 <div x-data="splash()" x-init="initSplash()">
-    <div x-ref="slideContainer" class="absolute left-0 top-0 transform duration-1000 flex justify-center items-center h-screen w-full bg-gradient-to-tr from-blue-500 to-blue-500">
+    <div x-ref="slideContainer" class="absolute left-0 top-0 transform duration-1000 flex justify-center items-center h-screen w-full bg-gradient-to-tr from-primary-500 to-secundary-500">
         <div>
             <x-logo />
+        </div>
+    </div>
+
+    <div class="flex flex-col min-h-screen justify-center items-center w-full">
+        <div x-ref="logo" class="transform scale-0 duration-500">
+            <img src="{{ asset('img/logo-adote-um-dev-gradient.svg') }}" alt="Logo" class="w-40">
+        </div>
+        <div x-ref="logoTitle" class="text-primary-500 mt-4 font-semibold transform scale-0 text-3xl duration-1000">
+            <h1>AdoteUm.Dev</h1>
         </div>
     </div>
 </div>
@@ -10,22 +19,28 @@
     function splash(){
         return {
             initSplash(){
-                this.animate(this.$refs.slideContainer, ['skew-x-12', 'left-full'], 'add', () => {
-                    this.animate(this.$refs.slideContainer, ['skew-x-12'], 'remove')
+                document.body.classList.add('overflow-hidden')
+
+                this.animate(this.$refs.slideContainer, ['skew-x-12', 'left-full'], 'add', 1000, () => {
+                    this.animate(this.$refs.slideContainer, ['skew-x-12'], 'remove', 900, () => {
+                        this.animate(this.$refs.logo, ['scale-100'], 'add', 200, () => {
+                            this.animate(this.$refs.logoTitle, ['scale-100'], 'add', 300)
+                        })
+                    })
                 })
             },
-            animate(element, classList, type, callback){
+            animate(element, classList, type, time, callback){
                 setTimeout(() => {
                     if(type === 'add'){
-                        this.$refs.slideContainer.classList.add(...classList)
+                        element.classList.add(...classList)
                     }else{
-                        this.$refs.slideContainer.classList.remove(...classList)
+                        element.classList.remove(...classList)
                     }
 
                     if(callback){
                         callback()
                     }
-                }, 1000);
+                }, time);
             }
         }
     }
