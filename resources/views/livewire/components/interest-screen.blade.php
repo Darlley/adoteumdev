abda499d0aa8e058fcf18cf33126a14b69c158f2
Darlley <systembrtt@gmail.com>
2022-08-31 17:39:08 -0400

    :lipstick: selections

--- resources/views/livewire/components/interest-screen.blade.php
+++ resources/views/livewire/components/interest-screen.blade.php
@@ -19,25 +19,23 @@
                     <div class="grid grid-cols-6 gap-6" >
 
                         <template x-for='category in categories'>
-
                             <div class="col-span-6 sm:col-span-3">
                                 <label for="country" class="block text-sm font-medium text-gray-700" x-text="category.name"></label>
-                                <select id="country" name="country" autocomplete="country-name" class="mt-1 block w-full py-2 px-3 border rounded-md focus:outline-none focus:ring-primary-500 focus:border-primary-500 sm:text-sm" x-on:change="changeSkill(category.name, $event)">
-                                    <option>Selecionar</option>
+                                <select id="country" name="country" autocomplete="country-name" class="mt-1 block w-full py-2 px-3 border rounded-md focus:outline-none focus:ring-primary-500 focus:border-primary-500 sm:text-sm" x-ref="selectionAlpine" x-on:change="changeSkill(category.name, $event)">
+                                    <option value="0">Selecionar</option>
                                     <template x-for='skill in category.skills'>
                                         <option :value="skill['id']" x-text="skill.name"></option>
                                     </template>
                                 </select>
-
                                 <div class="mt-2 space-y-2 rounded-md text-sm">
                                     <template x-if='payload.hasOwnProperty(category.name)' x-for='skill in payload[category.name]'>
-                                        <div class="flex border border-gray-400 rounded-md overflow-hidden font-bold">
+                                        <div class="flex border border-primary-500 rounded-md overflow-hidden font-bold">
                                             <div class="bg-gradient-to-l from-primary-500 to-secundary-500 text-white w-[80px] flex justify-center items-center">
-                                                <span x-text="skill.name" class=""></span>
+                                                <span x-text="skill.name"></span>
                                             </div>
-                                            <div class="flex-1 p-2 mx-2">
+                                            <div class="flex-1 p-2 mx-2 gap-1 flex flex-col">
                                                 <p x-text="skill.name" class="text-primary-500"></p>
-                                                <div class="text-gray-400">
+                                                <div class="text-gray-400 flex gap-1">
 
                                                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-4 h-4">
                                                         <path stroke-linecap="round" stroke-linejoin="round" d="M11.48 3.499a.562.562 0 011.04 0l2.125 5.111a.563.563 0 00.475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 00-.182.557l1.285 5.385a.562.562 0 01-.84.61l-4.725-2.885a.563.563 0 00-.586 0L6.982 20.54a.562.562 0 01-.84-.61l1.285-5.386a.562.562 0 00-.182-.557l-4.204-3.602a.563.563 0 01.321-.988l5.518-.442a.563.563 0 00.475-.345L11.48 3.5z" />
@@ -52,9 +50,7 @@
                                         </div>
                                     </template>
                                 </div>
-                                
                             </div>
-
                         </template>
 
                     </div>
@@ -90,6 +86,8 @@
                             level: 0
                         }]
                     }
+
+                    // $refs.selection.value = "" 
                 }
             }
         }
