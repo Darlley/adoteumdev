d86b1374d3983d3ac39418e6961f2d6e041413d7
Darlley <systembrtt@gmail.com>
2022-09-02 09:03:55 -0400

    :sparkles: remove skill

--- resources/views/livewire/components/interest-screen.blade.php
+++ resources/views/livewire/components/interest-screen.blade.php
@@ -20,14 +20,14 @@
                     <template x-for='category in categories'>
                         <div class="col-span-6 sm:col-span-3">
                             <label for="country" class="block text-sm font-medium text-gray-700" x-text="category.name"></label>
-                            <select id="country" name="country" autocomplete="country-name" class="mt-1 block w-full py-2 px-3 border rounded-md focus:outline-none focus:ring-primary-500 focus:border-primary-500 sm:text-sm" x-ref="selectionAlpine" x-on:change="changeSkill(category.name, $event)">
+                            <select :id="category.name" name="country" autocomplete="country-name" class="mt-1 block w-full py-2 px-3 border rounded-md focus:outline-none focus:ring-primary-500 focus:border-primary-500 sm:text-sm" x-on:change="changeSkill(category.name, $event)">
                                 <option value="0">Selecionar</option>
                                 <template x-for='skill in category.skills'>
                                     <option :value="skill['id']" x-text="skill.name"></option>
                                 </template>
                             </select>
                             <div class="mt-2 space-y-2 rounded-md text-sm">
-                                <template x-if='payload.hasOwnProperty(category.name)' x-for='skill in payload[category.name]'>
+                                <template x-if='payload.hasOwnProperty(category.name)' x-for='(skill, position) in payload[category.name]'>
                                     <div class="flex border border-primary-500 rounded-md overflow-hidden font-bold">
                                         <div class="bg-gradient-to-l from-primary-500 to-secundary-500 text-white w-[80px] flex justify-center items-center">
                                             <span x-text="skill.name"></span>
@@ -38,7 +38,7 @@
                                                 <template x-for='i in 5'>
                                                     <button x-on:click='skill.level = i'>
                                                         <template x-if="skill.level < i">
-                                                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-4 h-4 text-gray-400">
+                                                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-4 h-4 text-primary-300">
                                                                 <path stroke-linecap="round" stroke-linejoin="round" d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12z" />
                                                             </svg>
                                                         </template>
@@ -49,12 +49,13 @@
                                                             </svg>
                                                         </template>
                                                     </button>
+
                                                 </template>
                                             </div>
                                         </div>
 
                                         <div class="flex items-center p-2">
-                                            <button class="text-gray-500 hover:text-gray-600">
+                                            <button class="text-gray-500 hover:text-gray-600" x-on:click='removeSkill(category.name, position)'>
                                                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-4 h-4">
                                                     <path stroke-linecap="round" stroke-linejoin="round" d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0" />
                                                 </svg>
@@ -99,7 +100,12 @@
                         }]
                     }
 
-                    // $refs.selection.value = "" 
+                    const newCategory = category.filter((item) => item.id !== skill)
+
+                    document.querySelector(`#${selectedCategory}`).value = '0'
+                },
+                removeSkill(payloadCategory, position){
+                    this.payload[payloadCategory].splice(position, 1)
                 }
             }
         }
